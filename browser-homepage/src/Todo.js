b7d6cb37ab10e3425ab63fdb4ab7eed00126dad5
Harrison Marshall <hmar0007@student.monash.edu>
2020-04-02 22:28:27 +1100

    Added styling to todo list container

--- browser-homepage/src/Todo.js
+++ browser-homepage/src/Todo.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './Todo.css';
+import TodoItem from './TodoItem.js';
 
 function Todo() {
+
+    const [items, setItems] = React.useState([]);
+
     return (
         <div className="Todo">
+            <div className="TodoItem-list-header">
+                Todo List:
+            </div>
+            <div className="TodoItem-container">
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+            </div>
             <button class="new-item-button"><svg class="plus-icon" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
                 viewBox="0 0 492 492" >
                 <path d="M465.064,207.566l0.028,0H284.436V27.25c0-14.84-12.016-27.248-26.856-27.248h-23.116
---
+++ browser-homepage/src/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TodoItem.css';
+
+function TodoItem() {
+    return (
+        <div className="TodoItem">
+            
+        </div>
+    );
+}
+
+export default TodoItem;
