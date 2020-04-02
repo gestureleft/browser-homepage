bf91e387fc1580da7546942ad4cb6045212d8647
Harrison Marshall <hmar0007@student.monash.edu>
2020-04-02 20:53:40 +1100

    Added Todo component with some initial styling

--- browser-homepage/src/App.js
+++ browser-homepage/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Todo from './Todo.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Todo />
     </div>
   );
 }
