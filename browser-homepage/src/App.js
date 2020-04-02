9bd2a0644677541b28f7c7620c2b237d6d3a12e7
Harrison Marshall <hmar0007@student.monash.edu>
2020-04-02 21:37:27 +1100

    fixed bug where className was replaced with class

--- browser-homepage/src/App.js
+++ browser-homepage/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <svg class="background-circle" xmlns="http://www.w3.org/2000/svg" width="888" height="888" viewBox="0 0 888 888">
+      <svg className="background-circle" xmlns="http://www.w3.org/2000/svg" width="888" height="888" viewBox="0 0 888 888">
         <circle id="Ellipse_1" data-name="Ellipse 1" cx="444" cy="444" r="444" fill="#f2be42" />
       </svg>
       <Todo />
