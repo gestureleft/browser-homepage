4a022313f880c2701cecada88c349e8330b00ee0
Harrison Marshall <hmar0007@student.monash.edu>
2020-07-20 15:46:15 +1000

    Added build directory for web hosting

---
+++ browser-homepage/build/precache-manifest.9d48f3d3d250adb5f79a296d7cea5f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ab2310dff1dfa509b7e9db38cb1387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1f0bad5bb554435e824",
+    "url": "/static/css/main.799a58b4.chunk.css"
+  },
+  {
+    "revision": "1e381979caf1d8ad6961",
+    "url": "/static/js/2.cee3afb9.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/static/js/2.cee3afb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f0bad5bb554435e824",
+    "url": "/static/js/main.4d0dd4df.chunk.js"
+  },
+  {
+    "revision": "763d1a296cd70530bfb6",
+    "url": "/static/js/runtime-main.4241e0b2.js"
+  }
+]);
